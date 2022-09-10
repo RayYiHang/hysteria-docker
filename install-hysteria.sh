d1a69b2fb639bf0548c1075c038d2ebb589b542e
fs8vsx59h <106460965+fs8vsx59h@users.noreply.github.com>
2022-09-10 12:41:57 +0800

    Update install-hysteria.sh

--- install-hysteria.sh
+++ install-hysteria.sh
@@ -4,9 +4,9 @@
 apt -y update 
 apt -y upgrade
 apt -y install wget 
-wget https://github.com/HyNetwork/hysteria/releases/download/v1.1.0/hysteria-tun-linux-amd64
-mv ./hysteria-tun-linux-amd64 /bin/hysteria-tun-linux-amd64
-chmod +x /bin/hysteria-tun-linux-amd64
+wget https://github.com/HyNetwork/hysteria/releases/download/v1.2.1/hysteria-linux-amd64
+mv ./hysteria-linux-amd64 /bin/hysteria-linux-amd64
+chmod +x /bin/hysteria-linux-amd64
 mkdir /etc/hysteria
 mkdir /etc/hysteria/cert
 touch /etc/hysteria/config.json
